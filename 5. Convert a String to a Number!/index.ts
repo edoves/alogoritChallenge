7f6f63a968a9a0c0a0e1b5b4a3a48a7ed47d9460
dove <ndoverte11@gmail.com>
2021-02-21 17:40:55 +0800

    Solved the Convert a String to a Number challenge

---
+++ 5. Convert a String to a Number!/index.ts
@@ -0,0 +1,9 @@
+var stringToNumber = function (str: string) {
+  // put your code here
+  return parseInt(str);
+};
+
+console.log(stringToNumber("1234"));
+console.log(stringToNumber("605"));
+console.log(stringToNumber("1405"));
+console.log(stringToNumber("-7"));
