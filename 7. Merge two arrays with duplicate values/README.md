8bff2c816af6f57ce79b4f83feaa1cf58144c720
dove <ndoverte11@gmail.com>
2021-02-21 20:04:33 +0800

    Solved Merge two arrays with duplicate values challenge

---
+++ 7. Merge two arrays with duplicate values/README.md
@@ -0,0 +1,13 @@
+### jschallenger.com Merge two arrays with duplicate values
+
+---
+
+// Write a function that takes two arrays as arguments
+// Merge both arrays and remove duplicate values
+// Sort the merge result in ascending order
+// Return the resulting array
+
+**For example:**
+
+myFunction([1, 2, 3], [3, 4, 5]) --> [ 1, 2, 3, 4, 5 ]
+myFunction([-10, 22, 333, 42], [-11, 5, 22, 41, 42]) --> [ -11, -10, 5, 22, 41, 42, 333]
---
+++ 7. Merge two arrays with duplicate values/index.ts
@@ -0,0 +1,23 @@
+//ES 5 Syntax Possible Solution:
+function combine1(arr1: number[], arr2: number[]) {
+  const combineArray = arr1.concat(arr2);
+  const newArr = [];
+  for (let i = 0; i < combineArray.length; i++) {
+    if (newArr.indexOf(combineArray[i]) === -1) {
+      newArr.push(combineArray[i]);
+    }
+  }
+  return newArr;
+}
+
+console.log(combine1([1, 2, 3], [3, 4, 5]));
+console.log(combine1([-10, 22, 333, 42], [-11, 5, 22, 41, 42]));
+
+// ES 6 Syntax Possible Solution:
+const combine2 = (arr1: number[], arr2: number[]) => {
+  const merge = arr1.concat(arr2);
+  const newArr = merge.filter((item, pos) => merge.indexOf(item) === pos);
+  return newArr.sort((a, b) => a - b);
+};
+console.log(combine2([1, 2, 3], [3, 4, 5]));
+console.log(combine2([-10, 22, 333, 42], [-11, 5, 22, 41, 42]));
